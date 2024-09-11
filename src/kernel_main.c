6e385ea48359bacc3edb98fea1860a2f08b154a5
sljorg <121045665+sljorg@users.noreply.github.com>
2024-09-11 17:28:39 -0500

    Update kernel_main.c

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,8 +1,11 @@
-
+unsigned long get_timer_count() {
+ unsigned long *timer_count_register = 0x3f003004;
+ return *timer_count_register;
+}
 
 
 void kernel_main() {
-
+    get_timer_count();
     while(1){
     }
 }
