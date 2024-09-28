a4ea80580ef319f06e6a2fa28c74698cb7e4da7a
sljorg <121045665+sljorg@users.noreply.github.com>
2024-09-27 23:54:52 -0500

    Update serial.c

--- src/serial.c
+++ src/serial.c
@@ -1,6 +1,8 @@
+void putc(int data){
+  int *ptr = (int*)0x3F215040;
+  *ptr = data;
+}
 
-void putc(int data);
-
-void main{
+void main() {
   esp_printf(putc, "Current Execution Level is %d\r\n", getEL());
 }
