79923e43834566ab0c8b8042d1a222b396dc0031
sarah-jorgensen1 <121045665+sarah-jorgensen1@users.noreply.github.com>
2024-10-11 21:55:34 -0500

    Update page.c

--- src/page.c
+++ src/page.c
@@ -1,1 +1,25 @@
+struct ppage physical_page_array[128]; // 128 pages, each 2mb in length covers 256 megs of memory
+struct ppage *free_phys_pages;
 
+void init_pfa_list(void) { 
+  for(int i=0; i<128; i++){
+    list_add(free_phys_pages, physical_page_array[i]);
+    physical_page_array[i].physical_addr = &physical_page_array[i];
+    if(i>0){
+      physical_page_array[i-1].next = &physical_page_array[i];
+      physical_page_array[i].prev = &physical_page_array[i-1];
+    }
+  }
+}
+
+struct ppage *allocate_physical_pages(unsigned int npages) {
+  struct ppage *allocd_list;
+  for(int i=0; i<npages; i++){
+    list_add(allocd_list, physical_page_array[i]);
+  }
+  return allocd_list;
+}
+
+void free_physical_pages(struct ppage *ppage_list){
+  list_add(free_phys_pages, ppage_list);
+}
